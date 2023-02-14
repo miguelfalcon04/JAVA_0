d3fcf0278704f5f35b7d89f627ff0aa3657d8ed5
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:12:11 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje02.java
@@ -0,0 +1,19 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 2
+Modifica el programa anterior para que además se muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+separadas.
+
+@author Miguel Falcón
+*/
+
+public class Eje02 {
+    public static void main(String[] args) {
+
+        System.out.println("Miguel Falcón");
+        System.out.println("Dir: Calle Larios");
+        System.out.println("Tlf: 123 456 789");
+
+    }
+}
