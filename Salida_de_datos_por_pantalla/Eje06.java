01baeb190c5d5038b26380a06e8649cdf6072049
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:29:53 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje06.java
@@ -0,0 +1,20 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 6
+Escribe un programa que pinte por pantalla una pirámide rellena a base de
+asteriscos. La base de la pirámide debe estar formada por 9 asteriscos.
+
+@author Miguel Falcón
+*/
+
+public class Eje06 {
+    public static void main(String[] args) {
+
+    System.out.println("    *");
+    System.out.println("   ***");
+    System.out.println("  *****");
+    System.out.println(" *******");
+    System.out.println("*********");
+
+    }
+}
