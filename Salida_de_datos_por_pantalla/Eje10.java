d34906816b0ee685730b781298fb861f3a081def
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:40:32 +0100

    commit

--- Salida_de_datos_por_pantalla/Eje10.java
+++ Salida_de_datos_por_pantalla/Eje10.java
@@ -10,12 +10,9 @@ public class Eje10 {
     public static void main(String[] args) {
 
         String celeste = "\033[36m";
-        String verde = "\033[32m";
         String azul = "\033[34m";
         String rojo = "\033[31m";
         String morado = "\033[35m";
-        String naranja = "\033[33m";
-        String blanco = "\033[37m";   
 
         System.out.println(rojo+"      BIENVENIDO");
         System.out.println(morado+"   A ESTE EJERCICIO\n");
