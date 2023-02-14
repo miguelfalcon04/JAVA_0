1262ba13e95d9af5140d3981eb61343b3deee5fe
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:34:34 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje08.java
@@ -0,0 +1,20 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 8
+Igual que el programa anterior, pero esta vez la pirámide debe aparecer
+invertida, con el vértice hacia abajo.
+
+@author Miguel Falcón
+*/
+
+public class Eje08 {
+    public static void main(String[] args) {
+
+        System.out.println("*********");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+
+    }
+}
