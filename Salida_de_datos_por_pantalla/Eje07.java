8944b0ed84749d95f495c3c82a93c9492a3f993c
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:32:46 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje07.java
@@ -0,0 +1,20 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 7
+Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe
+ver únicamente el contorno hecho con asteriscos).
+
+@author Miguel Falcón
+*/
+
+public class Eje07 {
+    public static void main(String[] args) {
+
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println(" *     *");
+        System.out.println("*********");
+    
+    }
+}
