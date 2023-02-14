89e2c2169b9e382ce91d1b1490e92e236c1e7f1d
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:10:11 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje01.java
@@ -0,0 +1,14 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 1
+Escribe una programa que muestre tu nombre por pantalla
+
+@author Miguel Falcón
+*/
+public class Eje01{
+    public static void main(String[] args) {
+
+        System.out.print("Miguel Falcon");
+
+    }
+}
