7d6935d5c4ae177e97d27dddd2b6c573c0a11b34
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:17:46 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje04.java
@@ -0,0 +1,22 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 4
+Escribe un programa que muestre tu horario de clase.
+
+@author Miguel Falcón
+*/
+
+public class Eje04 {
+    public static void main(String[] args) {
+
+        System.out.println("\n Lunes\tMartes\tMiérc.\tJueves.\tViernes");
+        System.out.println(" ======\t=======\t======\t=======\t=======");
+        System.out.println(" PROG\tSIN\tED\tPROG \tFOL");
+        System.out.println(" PROG\tSIN\tED\tPROG\tFOL");
+        System.out.println(" LM\tSIN\tPROG\tLM\tFOL");
+        System.out.println(" LM\tSIN\tPROG\tLM\tSIN");
+        System.out.println(" BD\tPROG\tED\tBDATO\tSIN");
+        System.out.println(" BD\tED\tED\tBDATO\tSIN");
+
+    }
+}
