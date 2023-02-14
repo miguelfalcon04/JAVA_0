44dcbb120efef947536bc1e347cb82f55072fe03
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-14 13:36:26 +0100

    commit

---
+++ Salida_de_datos_por_pantalla/Eje09.java
@@ -0,0 +1,27 @@
+package Salida_de_datos_por_pantalla;
+
+/* Ejercicio 9
+Escribe un programa que pinte por pantalla alguna escena - el campo, la
+habitación de una casa, un aula, etc. - o algún objeto animado o inanimado
+- un coche, un gato, una taza de café, etc. Ten en cuenta que puedes utilizar
+caracteres como *, +, <, #, @, etc. o incluso caracteres Unicode. ¡Échale
+imaginación!
+
+@author Miguel Falcón
+*/
+
+public class Eje09 {
+    public static void main(String[] args) {
+
+        System.out.println("      BIENVENIDO");
+        System.out.println("   A ESTE EJERCICIO\n");
+        System.out.println("  Miguel Jeús Falcón");
+        System.out.println(".............");
+        System.out.println(".           .");
+        System.out.println(". O      O  .");
+        System.out.println(".           .");
+        System.out.println(".   (---)   .");
+        System.out.println(".............");
+        
+    }
+}
