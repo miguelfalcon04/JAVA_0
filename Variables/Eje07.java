70b59b557897cba1b58e275010e7a37d67965457
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-15 10:07:19 +0100

    commit

---
+++ Variables/Eje07.java
@@ -0,0 +1,20 @@
+package Variables;
+
+/* Ejercicio 7
+Escribe un programa que declare variables de tipo char y de tipo String. Intenta
+mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia
+de Java (con un solo println) ¿es posible?
+
+@author Miguel Falcón
+*/
+
+public class Eje07 {
+    public static void main(String[] args) {
+
+        char letra = 'a';
+        String palabra = "palabra";
+
+        System.out.print(letra+" "+palabra);
+
+    }
+}
