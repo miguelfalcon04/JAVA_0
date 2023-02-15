e972fa8e0684de731b1923d25442e4f69d5ff5de
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-15 10:06:29 +0100

    commit

---
+++ Variables/Eje03.java
@@ -0,0 +1,22 @@
+package Variables;
+
+/* Ejercicio 3
+Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. Muestra los valores de esas variables 
+por pantalla de tal forma que el resultado del programa sea el mismo que en el ejercicio 2.
+
+@author Miguel Falcón
+*/
+
+public class Eje03 {
+    public static void main(String[] args) {
+
+        String nombre= "Miguel Falcón";
+        String direccion= "Dir= Calle Larios";
+        String telefono= "Tlf= 123 456 789";
+
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+
+    }
+}
