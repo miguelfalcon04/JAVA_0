b1feea4533db131f478b71fc4f30b25f6402b51f
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-26 13:36:00 +0100

    commit

---
+++ Bucles/Eje11.java
@@ -0,0 +1,25 @@
+package Bucles;
+
+/* Ejercicio 11
+Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
+los 5 primeros números enteros a partir de uno que se introduce por teclado.
+
+@author Miguel Falcón
+*/
+
+import java.util.Scanner;
+public class Eje11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduzca un número: ");
+        int n = sc.nextInt();
+
+        for(int i = n; i<n+5; i++){
+            System.out.println(i+" "+i*i+" "+i*i*i);
+        }
+
+        sc.close();
+    }
+}
