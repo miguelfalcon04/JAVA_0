1ac0802177f0db8d21c65a17dba0db8eafdb74f4
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-26 13:36:53 +0100

    commit

---
+++ Bucles/Eje13.java
@@ -0,0 +1,35 @@
+package Bucles;
+
+/* Ejercicio 13
+Escribe un programa que lea una lista de diez números y determine cuántos
+son positivos, y cuántos son negativos.
+
+@author Miguel Falcón
+*/
+
+import java.util.Scanner;
+public class Eje13 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int pos=0;
+        int neg=0;
+
+        for(int i=0; i<10; i++){
+
+            System.out.print("Introduzca un número: ");
+            int n = sc.nextInt();
+
+            if(n>0){
+                pos++;
+            }else{
+                neg++;
+            }
+        }
+
+        System.out.printf("Hay %d positivos y %d negativos",pos,neg);
+
+        sc.close();
+    }
+}
