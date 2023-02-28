eb94dd04f760fb88faff95509246c1338ff39c86
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-28 17:58:32 +0100

    commit

---
+++ Bucles/Eje24.java
@@ -0,0 +1,16 @@
+package Bucles;
+
+/* Ejercicio 24
+Escribe un programa que lea un número n e imprima una pirámide de números
+con n filas como en la siguiente figura:
+
+@author Miguel Falcón
+*/
+
+import java.util.Scanner;
+public class Eje24 {
+    public static void main(String[] args) {
+
+
+    }
+}
