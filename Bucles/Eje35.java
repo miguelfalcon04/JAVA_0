48358315a949927ad51b929e9791ecea32e6e46f
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-03-03 13:58:29 +0100

    commit

---
+++ Bucles/Eje35.java
@@ -0,0 +1,16 @@
+package Bucles;
+
+/* Ejercicio 35
+Realiza un programa que pinte una X hecha de asteriscos. El programa debe
+pedir la altura. Se debe comprobar que la altura sea un número impar mayor o
+igual a 3, en caso contrario se debe mostrar un mensaje de error.
+
+@author Miguel Falcón
+*/
+
+import java.util.Scanner;
+public class Eje35 {
+    public static void main(String[] args) {
+        
+    }
+}
