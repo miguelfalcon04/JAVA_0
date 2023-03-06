a6425e1c20fe5a35113be0f44d7582c16c67ac6f
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-03-06 17:46:14 +0100

    commit

---
+++ Bucles/Eje40.java
@@ -0,0 +1,19 @@
+package Bucles;
+
+/* Ejercicio 40
+Realiza un programa que pinte por pantalla un rombo hueco hecho con
+asteriscos. El programa debe pedir la altura. Se debe comprobar que la altura
+sea un número impar mayor o igual a 3, en caso contrario se debe mostrar un
+mensaje de error.
+
+@author Miguel Falcón
+*/
+
+import java.util.Scanner;
+public class Eje40 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
