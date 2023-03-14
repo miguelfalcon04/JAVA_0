1837dce456ebaf8d0194fd7b487446f9f9af9acb
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-03-14 11:55:53 +0100

    commit

---
+++ Arrays/Eje01.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+public class Eje01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n [] = new int [8];
+
+        for(int i=0; i<n.length; i++){
+            System.out.print("Introduzca un valor: ");
+            n[9] = sc.nextInt();
+        }
+        
+
+        sc.close();
+    }
+}
